d9526327a1211f4162201ec1d7b1a7e2e1325d98
Zhengkang Chen <70932380+kckevinchenzkk@users.noreply.github.com>
2024-02-13 00:35:49 -0500

    done caching

--- playground/urls.py
+++ playground/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 # URLConfiguration
 urlpatterns = [
-    path("hello/", views.say_hello)
+    path("hello/", views.HelloView.as_view())
 ]
+
+# urlpatterns = [
+#     path("hello/", views.say_hello)
+# ]
--- playground/views.py
+++ playground/views.py
@@ -15,10 +15,34 @@ from django.core.mail import EmailMessage, send_mail, mail_admins, BadHeaderErro
 from templated_mail.mail import BaseEmailMessage
 from .tasks import notify_customers
 
+from django.core.cache import cache
+from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from rest_framework.views import APIView
+import requests
 
-def say_hello(request):
-    notify_customers.delay('Hello')
-    return render(request, 'hello.html', {'name': 'Mosh'})
+# slow api
+class HelloView(APIView):
+    @method_decorator(cache_page(5 * 60))
+    def get(self, request):
+        response = requests.get('https://httpbin.org/delay/2')
+        data = response.json()
+        return render(request, 'hello.html', {'name': 'Mosh'})
+# caching
+# def say_hello(request):
+#     key = 'httpbin_result'
+#     if cache.get(key) is None:
+#         response = request.get('https://httpbin.org/delay/2')
+#         data = response.json()
+#         cache.set(key,data)
+#     return render(request, 'hello.html', {'name': cache.get(key)})
+
+# sendingemails
+
+# def say_hello(request):
+#     notify_customers.delay('Hello')
+#     return render(request, 'hello.html', {'name': 'Mosh'})
 # 
 # def say_hello(request):
 #     try:
@@ -69,63 +93,63 @@ def say_hello(request):
     # queryset = Product.objects.prefetch_related('promotions').all()
     # queryset = Product.objects.prefetch_related('promotions').select_related('collection').all()
     # queryset = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
-    result = Product.objects.filter(collection__id=3).aggregate(count = Count('id'), min_price = Min('unit_price'))
-    queryset = Customer.objects.annotate(is_new=Value(True))
-    queryset = Customer.objects.annotate(new_id = F('id'))
+    # result = Product.objects.filter(collection__id=3).aggregate(count = Count('id'), min_price = Min('unit_price'))
+    # queryset = Customer.objects.annotate(is_new=Value(True))
+    # queryset = Customer.objects.annotate(new_id = F('id'))
+    # # queryset = Customer.objects.annotate(
+    # #     # CONCAT
+    # #     full_name = Func(F('first_name'), Value(' '), F('last_name'), function='CONCAT' )
+    # # )
     # queryset = Customer.objects.annotate(
     #     # CONCAT
-    #     full_name = Func(F('first_name'), Value(' '), F('last_name'), function='CONCAT' )
+    #     full_name = Concat('first_name', Value(' '), 'last_name')
     # )
-    queryset = Customer.objects.annotate(
-        # CONCAT
-        full_name = Concat('first_name', Value(' '), 'last_name')
-    )
-    queryset = Customer.objects.annotate(
-        orders_count = Count('order')
-    )
-    discounted_price = ExpressionWrapper(F('unit_price') * 0.8, output_field=FloatField())
-    queryset = Product.objects.annotate(
-        discounted_price = discounted_price
-    )
+    # queryset = Customer.objects.annotate(
+    #     orders_count = Count('order')
+    # )
+    # discounted_price = ExpressionWrapper(F('unit_price') * 0.8, output_field=FloatField())
+    # queryset = Product.objects.annotate(
+    #     discounted_price = discounted_price
+    # )
 
    
-    TaggedItem.objects.get_tags_for(Product,1)
+    # TaggedItem.objects.get_tags_for(Product,1)
 
-    queryset = Product.objects.all()
-    list(queryset)
-    # objects:::::::
-    #return render(request, 'hello.html', {'name': 'Kevin', 'products': list(queryset)})
-    # collection = Collection(pk = 11)
-    # collection.title = 'Games'
-    # collection.featured_product = Product(pk=1)
-    # collection.save()
+    # queryset = Product.objects.all()
+    # list(queryset)
+    # # objects:::::::
+    # #return render(request, 'hello.html', {'name': 'Kevin', 'products': list(queryset)})
+    # # collection = Collection(pk = 11)
+    # # collection.title = 'Games'
+    # # collection.featured_product = Product(pk=1)
+    # # collection.save()
     
-    #Collection.objects.filter(pk=11).update(featured_product= None)
-    # collection = Collection(pk=11)
-    # collection.delete()
-    # Collection.objects.filter(id__gt = 5).delete()
+    # #Collection.objects.filter(pk=11).update(featured_product= None)
+    # # collection = Collection(pk=11)
+    # # collection.delete()
+    # # Collection.objects.filter(id__gt = 5).delete()
 
-    # with transaction.atomic():
-    #     order = Order()
-    #     order.customer_id = 1
-    #     order.save()
+    # # with transaction.atomic():
+    # #     order = Order()
+    # #     order.customer_id = 1
+    # #     order.save()
 
-    #     item = OrderItem()
-    #     item.order = order
-    #     item.product_id = 1
-    #     item.quantity = 1
-    #     item.unit_price = 10
-    #     item.save()
+    # #     item = OrderItem()
+    # #     item.order = order
+    # #     item.product_id = 1
+    # #     item.quantity = 1
+    # #     item.unit_price = 10
+    # #     item.save()
 
-    # cursor = connection.cursor()
-    # cursor.execute('')
-    # cursor.close()
+    # # cursor = connection.cursor()
+    # # cursor.execute('')
+    # # cursor.close()
 
-    # with connection.cursor as cursor:
-    #     cursor.execute()
-    #     cursor.callproc('get_customers', [1,2,'a'])
+    # # with connection.cursor as cursor:
+    # #     cursor.execute()
+    # #     cursor.callproc('get_customers', [1,2,'a'])
     
-    # queryset = Product.objects.raw('SELECT id, title FROM store_product')
+    # # queryset = Product.objects.raw('SELECT id, title FROM store_product')
 
 
-    return render(request, 'hello.html', {'name': 'Kevin', 'result':  result, 'orders': list(queryset)})
+    # #return render(request, 'hello.html', {'name': 'Kevin', 'result':  result, 'orders': list(queryset)})
