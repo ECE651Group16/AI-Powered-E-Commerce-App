4fbe6a7e68fd2e813973b8a1795c7805353248a7
Zhengkang Chen <70932380+kckevinchenzkk@users.noreply.github.com>
2024-02-26 19:47:05 -0500

    add localhost:3000

--- storefront/settings/common.py
+++ storefront/settings/common.py
@@ -69,7 +69,10 @@ INTERNAL_IPS = [
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:8001',
     'http://127.0.0.1:8001',
+    'http://127.0.0.1:3000',
+    'http://localhost:3000',
 ]
+
 
 ROOT_URLCONF = 'storefront.urls'
 
