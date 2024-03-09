cd244535a7f16a43f5c6ac47800abaacd2df469a
Zhengkang Chen <70932380+kckevinchenzkk@users.noreply.github.com>
2024-03-09 17:39:56 -0500

    adding port 3000 for cors

--- backend/storefront/settings/common.py
+++ backend/storefront/settings/common.py
@@ -69,6 +69,8 @@ INTERNAL_IPS = [
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:8001',
     'http://127.0.0.1:8001',
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
 ]
 
 ROOT_URLCONF = 'storefront.urls'
