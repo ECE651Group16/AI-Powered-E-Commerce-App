067672808ef7444b5306422ed94e0284be7a3d27
Luwen Wang <l256wang@uwaterloo.ca>
2024-03-10 23:02:38 -0400

    connection btw likes and cart

--- backend/likes/views.py
+++ backend/likes/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from django.core.exceptions import ValidationError
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin, DestroyModelMixin
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from .models import Like, LikedItem
 from .serializer import AddLikedItemSerializer,LikedItemSerializer,LikeSerializer
+from store.serializers import CartSerializer
 # Create your views here.
 
 
@@ -31,3 +36,13 @@ class LikedItemViewSet(ModelViewSet):
         return LikedItem.objects \
             .filter(like_id=self.kwargs['like_pk']) \
             .select_related('product')
+            
+    @action(detail=True, methods=['post'], url_path='add-to-cart')
+    def add_to_cart(self, request, *args, **kwargs):
+        try:
+            liked_item = self.get_object()
+            cart, cart_item = liked_item.add_to_cart()
+            serializer = CartSerializer(cart, context={'request': request})
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        except ValidationError as e:
+            return Response({'detail': str(e)}, status=status.HTTP_400_BAD_REQUEST)
