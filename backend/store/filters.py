ab6ea688068fdefc0c90eba847f8c1553464820b
Zhengkang Chen <70932380+kckevinchenzkk@users.noreply.github.com>
2024-03-15 16:07:12 -0400

    update django filter

--- backend/store/filters.py
+++ backend/store/filters.py
@@ -3,16 +3,17 @@ from django_filters import rest_framework as filters
 from .models import Product
 
 class ProductFilter(FilterSet):
-  class Meta:
-    model = Product
-    fields = {
-      'collection_id': ['exact'],
-      'unit_price': ['gt', 'lt']
-    }
-    # collection_id = filters.BaseInFilter(field_name='collection__id')
-    # unit_price_gt = filters.NumberFilter(field_name='unit_price', lookup_expr='gt')
-    # unit_price_lt = filters.NumberFilter(field_name='unit_price', lookup_expr='lt')
+  # class Meta:
+  #   model = Product
+  #   fields = {
+  #     'collection_id': ['exact'],
+  #     'unit_price': ['gt', 'lt']
+  #   }
 
-    # class Meta:
-    #     model = Product
-    #     fields = ['collection_id', 'unit_price']
+  collection_id = filters.BaseInFilter(field_name='collection_id', lookup_expr='in')
+  # unit_price_gt = filters.NumberFilter(field_name='unit_price', lookup_expr='gt')
+  # unit_price_lt = filters.NumberFilter(field_name='unit_price', lookup_expr='lt')
+
+  class Meta:
+      model = Product
+      fields ={'unit_price': ['gt', 'lt']}
