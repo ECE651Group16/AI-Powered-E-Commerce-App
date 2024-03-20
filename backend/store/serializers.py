101adce2c027090b87fe3c1d9178d88bdd56fc31
Luwen Wang <l256wang@uwaterloo.ca>
2024-03-19 23:23:31 -0400

    review - user deleted

--- backend/store/serializers.py
+++ backend/store/serializers.py
@@ -39,7 +39,7 @@ class ReviewSerializer(serializers.ModelSerializer):
     def get_username(self, obj):
         if obj.customer and obj.customer.user:
             return obj.customer.user.username
-        return "User undefined."
+        return "User deleted."
 
     
 class UpdateReviewSerializer(serializers.ModelSerializer):
