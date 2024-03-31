ab6e595cc9c20d0a9b15fb7d2737df14271259ea
Luwen Wang <l256wang@uwaterloo.ca>
2024-03-31 00:47:30 -0400

    additional order status

--- backend/store/models.py
+++ backend/store/models.py
@@ -112,10 +112,14 @@ class Order(models.Model):
     PAYMENT_STATUS_PENDING = "P"
     PAYMENT_STATUS_COMPLETE = "C"
     PAYMENT_STATUS_FAILED = "F"
+    PAYMENT_STATUS_RECEIVED = "R"  
+    PAYMENT_STATUS_SHIPPED = "S"
     PAYMENT_STATUS_CHOICES = [
         (PAYMENT_STATUS_PENDING, "Pending"),
         (PAYMENT_STATUS_COMPLETE, "Complete"),
         (PAYMENT_STATUS_FAILED, "Failed"),
+        (PAYMENT_STATUS_RECEIVED, "Payment Received"),
+        (PAYMENT_STATUS_SHIPPED, "Shipped"),
     ]
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(
